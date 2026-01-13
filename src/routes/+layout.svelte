9d7f4aa8318e180f96b78e4459a849c18bea4e7a
Wing <114337707+wing-0902@users.noreply.github.com>
2026-01-13 13:12:17 +0000

    フォントとか

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
   import favicon from '$lib/assets/favicon.svg';
+  import '$lib/styles/global.scss';
+
+  // フォント
+  import '@fontsource/zen-kaku-gothic-new';
+  import '@fontsource-variable/fira-code';
+  import '@fontsource-variable/m-plus-1-code';
+  import '@fontsource/zen-maru-gothic';
 
   let { children } = $props();
 </script>
@@ -10,5 +17,6 @@
     href={favicon}
   />
 </svelte:head>
+
 
 {@render children()}
