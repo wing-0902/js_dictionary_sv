4ea0c12e5c56a9f52df66f4990f47be7ad8951c1
Wing <114337707+wing-0902@users.noreply.github.com>
2026-01-17 08:52:02 +0000

    [skip ci]vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import pkg from './package.json' assert { type: 'json' };
+import license from 'vite-plugin-license';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [
+    sveltekit(),
+    license({
+      thirdParty: {
+        output: {
+          file: path.resolve(__dirname, 'src/lib/data/license-dat.json'),
+          template(dependencies) {
+            return JSON.stringify(dependencies, null, 2);
+          },
+        },
+      },
+    }),
+  ],
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version),
     __APP_NAME__: JSON.stringify(pkg.name)
